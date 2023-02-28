e3034c44737ae00579a77b43460777513e8c7b4c
Dravide <derysupriady@gmail.com>
2023-02-28 16:27:16 +0700

    CRUD Kategori

---
+++ app/Http/Controllers/KategorisController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+
+class KategorisController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Kategori $kategori)
+    {
+    }
+
+    public function edit(Kategori $kategori)
+    {
+    }
+
+    public function update(Request $request, Kategori $kategori)
+    {
+    }
+
+    public function destroy(Kategori $kategori)
+    {
+    }
+}
